16233fda6500e836205f0ad7977246d5c20724b3
PUVHAM <phamquangvu19082005@gmail.com>
2024-08-10 02:03:28 -0700

    feat: add multi-threading support to add_embedding function

--- src/streamlit_app/chroma_client.py
+++ src/streamlit_app/chroma_client.py
@@ -1,14 +1,17 @@
 from tqdm import tqdm
-from PIL import Image
-from streamlit_app.utils import get_single_image_embedding
+from concurrent.futures import ThreadPoolExecutor
 
 def add_embedding(collection, files_path):
+    from streamlit_app.image_utils import process_image
+
     ids = []
     embeddings = []
-    for id_filepath, filepath in tqdm(enumerate(files_path)):
+    
+    with ThreadPoolExecutor() as executor:
+        results = list(tqdm(executor.map(process_image, files_path), total=len(files_path)))
+
+    for id_filepath, embedding in enumerate(results):
         ids.append(f'id_{id_filepath}')
-        image = Image.open(filepath)
-        embedding = get_single_image_embedding(image=image)
         embeddings.append(embedding)
     collection.add(
         embeddings=embeddings,
